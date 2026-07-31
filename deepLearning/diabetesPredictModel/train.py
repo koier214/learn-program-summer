f663931566a8bdf306b883f9f44bb035ea781224
koi <koier214@outlook.com>
2026-07-31 15:37:44 +0800

    增加mini-batch

--- deepLearning/diabetesPredictModel/train.py
+++ deepLearning/diabetesPredictModel/train.py
@@ -2,45 +2,59 @@ import os
 import torch
 import numpy as np
 from models.diabetes_model import Model
+from torch.utils.data import DataLoader, Dataset
 
 
 def main():
-    # 加载数据
-    xy = np.loadtxt('diabetes.csv.gz', delimiter=',', dtype=np.float32)
-    x_data = torch.from_numpy(xy[:, :-1])
-    y_data = torch.from_numpy(xy[:, [-1]])
+    class DiabetesDataset(Dataset):
+        def __init__(self, filepath):
+            xy = np.loadtxt(filepath, delimiter=',', dtype=np.float32)
+            self.len = xy.shape[0]
+            x_data = torch.from_numpy(xy[:, :-1])
+            y_data = torch.from_numpy(xy[:, [-1]])
 
-    # 归一化：保存 mean 和 std，predict.py 要用
-    mean = x_data.mean(dim=0)
-    std = x_data.std(dim=0)
-    x_data = (x_data - mean) / std
+            # 归一化在 Dataset 里做
+            self.mean = x_data.mean(dim=0)
+            self.std = x_data.std(dim=0)
+            self.x_data = (x_data - self.mean) / self.std
+            self.y_data = y_data
+
+        def __getitem__(self, index):
+            return self.x_data[index], self.y_data[index]
+
+        def __len__(self):
+            return self.len
+
+    dataset = DiabetesDataset('diabetes.csv.gz')
+    train_loader = DataLoader(dataset=dataset, batch_size=32, shuffle=True)
 
     model = Model()
 
-    criterion = torch.nn.BCELoss(reduction='sum')
+    criterion = torch.nn.BCELoss(reduction='mean')
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
     for epoch in range(1000):
-        y_pred = model(x_data)
-        loss = criterion(y_pred, y_data)
-        print(epoch, loss.item())
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+            y_pred = model(inputs)
+            loss = criterion(y_pred, labels)
+            print(f'Epoch {epoch}, Batch {i}, Loss: {loss.item()}')
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
     print('训练完成')
 
-    # 保存模型权重
     save_dir = os.path.join(os.path.dirname(__file__), 'checkpoints')
     os.makedirs(save_dir, exist_ok=True)
     save_path = os.path.join(save_dir, 'diabetes_model.pth')
     torch.save(model.state_dict(), save_path)
     print(f'模型已保存至 {save_path}')
 
-    # 保存归一化参数，predict.py 要用
+    # 归一化参数从 Dataset 对象中取
     norm_path = os.path.join(save_dir, 'norm_params.pth')
-    torch.save({'mean': mean, 'std': std}, norm_path)
+    torch.save({'mean': dataset.mean, 'std': dataset.std}, norm_path)
     print(f'归一化参数已保存至 {norm_path}')
 
 
